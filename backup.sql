770712f6ffcadf2321e15c6a89665532c1986808
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-16 20:11:51 +0700

    backup cmd syntax

---
+++ backup.sql
@@ -0,0 +1,7 @@
+
+-- Backup
+-- mysqldump go_sample --user root --password --result-file=/Users/FA/Desktop/backup.sql
+
+-- Restore
+-- mysql --user=root --password go_sample_clone < /Users/FA/Desktop/backup.sql
+-- SOURCE /Users/FA/Desktop/backup.sql
